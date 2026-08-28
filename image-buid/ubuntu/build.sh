18752b2b1e3095e4919cfea5f0a4d8084764a98c
Ricardo Caldeira <ricardohcaldeira@gmail.com>
2026-08-27 21:45:29 -0300

    imagem ubuntu-dind

--- image-buid/ubuntu/build.sh
+++ image-buid/ubuntu/build.sh
@@ -1,2 +1,3 @@
 docker build -t ricardohcaldeira/ubuntu-dind .
-#docker run -d --privileged --name dind -p 2376:2376 ricardohcaldeira/ubuntu-dind
+# upload to docker hub
+docker push ricardohcaldeira/ubuntu-dind:latest
