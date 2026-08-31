711dfd574854b2ecf9849907ffeb43f0b4609d6c
Ricardo Caldeira <ricardohcaldeira@gmail.com>
2026-08-31 19:21:13 -0300

    added rustfs

--- image-buid/alpine/build.sh
+++ image-buid/alpine/build.sh
@@ -3,10 +3,3 @@ docker build -t ricardohcaldeira/dind .
 
 # upload to docker hub
 docker push ricardohcaldeira/dind:latest
-
-# execução — o --privileged é essencial para o daemon do Docker funcionar
-#  docker run --privileged -it ricardohcaldeira/dind
-
-# dentro do container, teste:
-#docker info          # daemon rodando
-#infisical --version # CLI do Infisical
