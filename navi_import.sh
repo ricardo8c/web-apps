d66e71e92846bf68c0c496b0dad0a821ac7bbd61
Ricardo Caldeira <ricardohcaldeira@gmail.com>
2026-09-05 11:34:57 -0300

    imagens atualizadas

--- navi_import.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.local/share/navi/cheats/web-apps
-# importar as cheats para o navi local
-cp navi/*.cheat ~/.local/share/navi/cheats/web-apps
---
+++ start_navi.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.local/share/navi/cheats
+# importar as cheats para o navi local
+cp navi/*.cheat ~/.local/share/navi/cheats
+navi --path ./navi
